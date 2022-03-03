a7aba0e63141524aee39cb5075a1544c3fca54c7
programmERic <eweispfenning@gmail.com>
2022-03-03 11:06:26 -0600

    playing around with channel, go routine

--- main.go
+++ main.go
@@ -6,55 +6,61 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+
 	args := os.Args
 	filename := "example.txt"
+
 	if len(args) == 2 {
 		filename = args[1]
 	} else {
-		fmt.Println("No file provided. example.txt will be used.")
+		fmt.Printf("No file provided. %v will be used.\n", filename)
 	}
 
 	urls := obtainUrlsFromFile(filename)
-	fmt.Println(urls)
-
 	startMonitor(urls)
 }
 
 func startMonitor(urls []string) {
-	for {
 
-		for _, url := range urls {
-			v := checkSiteStatus(url)
+	status := make(chan string)
 
-			fmt.Println(v)
+	for i := 0; i < 2; i++ {
+		for _, u := range urls {
+			go checkSiteStatus(u, status)
 		}
-
+		fmt.Println(<-status)
+		fmt.Println()
 	}
 }
 
-func checkSiteStatus(s string) bool {
+func checkSiteStatus(s string, c chan string) {
 	r, err := http.Get(s)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	return r.StatusCode != 200
+	c <- fmt.Sprint(r.Status, " returned status from: ", s)
+	time.Sleep(5 * time.Second)
 }
 
 func obtainUrlsFromFile(filename string) []string {
+
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	fileContents := strings.ReplaceAll(string(data), "\r\n", "\n")
 	urls := strings.Split(fileContents, "\n")
 	if len(urls) == 0 {
-		fmt.Errorf("No urls obtained from %v", filename)
+		fmt.Printf("No urls obtained from %v", filename)
 		os.Exit(1)
 	}
+
 	return urls
 }
